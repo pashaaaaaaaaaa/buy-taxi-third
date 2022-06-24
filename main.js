d2fdb3eb8b2ca8ae3412c7a87713961cb3c0caa6
pasha <pashaganiev20@gmail.com>
2022-06-24 16:01:47 +0300

    addional mobile version

--- main.js
+++ main.js
@@ -58,3 +58,46 @@ let closeModal = document.querySelector('.btn__close')
 closeModal.addEventListener('click', ()=>{
     options.style.display = 'none'
 })
+
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+// when window width is >= 320px
+        320: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        slidesPerGroup: 1,
+
+        },
+        // when window width is >= 480px
+        767: {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        slidesPerGroup: 1,
+        },
+        768: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+        slidesPerGroup: 2,
+        },
+        1400: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        slidesPerGroup: 3,
+        }
+// when window width is >= 640px
+}
+    
+  });
